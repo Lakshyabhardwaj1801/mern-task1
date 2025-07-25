6818ac22308734d42d0660dfaf7800e243604aa5
Lakshya Bhardwaj <142743050+Lakshyabhardwaj1801@users.noreply.github.com>
2025-07-25 05:57:08 +0530

    Add files via upload

---
+++ validateProduct.js
@@ -0,0 +1,15 @@
+const { body, validationResult } = require('express-validator');
+
+const validateProduct = [
+  body('name').trim().notEmpty().withMessage('Name is required'),
+  body('price').isFloat({ gt: 0 }).withMessage('Price must be > 0'),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  }
+];
+
+module.exports = validateProduct;
